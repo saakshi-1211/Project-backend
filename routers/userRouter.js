0b32ce1e0415e81b08579c93af4514544ad55aad
Saaksshie <saakshigupta81@gmail.com>
2024-04-08 18:07:29 +0530

    intial commit

---
+++ routers/userRouter.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import userController from "../controllers/userController.js"
+const router = express.Router();
+
+// router.get('/', (req, res)=>{});
+// router.post('/', (req, res)=>{});
+
+router.route('/')
+    .get(userController.getUser)
+    .post(userController.createUser)
+    //mvc architecture
+
+export default router;
---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+import userRouter from "./routers/userRouter.js"
+import cors from 'cors';
+
+const app=express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/users', userRouter);
+
+app.listen(3000, ()=>{
+    console.log("http://localhost:3000");
+})
